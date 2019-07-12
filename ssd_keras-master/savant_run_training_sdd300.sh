35770729dba180d2d056e0b2f3b09ca746d095f8
omer <omer@lisuto.com>
2019-07-12 12:46:06 +0300

    changes

--- ssd_keras-master/savant_run_training_sdd300.sh
+++ ssd_keras-master/savant_run_training_sdd300.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 export IMAGES_DIR_2007=/specific/netapp5_2/gamir/datasets/VOCdevkit/VOC2007/JPEGImages
 export IMAGES_DIR_2007_TEST=/specific/netapp5_2/gamir/datasets/VOCdevkit/VOC2007TEST/JPEGImages
 export IMAGES_DIR_2012=/specific/netapp5_2/gamir/datasets/VOCdevkit/VOC2012/JPEGImages
@@ -7,6 +8,12 @@ export IMAGES_DIR_2012=/specific/netapp5_2/gamir/datasets/VOCdevkit/VOC2012/JPEG
 export ANNOT_DIR_2007=/specific/netapp5_2/gamir/datasets/VOCdevkit/VOC2007/Annotations
 export ANNOT_DIR_2007_TEST=/specific/netapp5_2/gamir/datasets/VOCdevkit/VOC2007TEST/Annotations
 export ANNOT_DIR_2012=/specific/netapp5_2/gamir/datasets/VOCdevkit/VOC2012/Annotations
+
+export IMAGESET_TRAIN_2007=datasets/VOCdevkit/VOC2007/ImageSets/Main/train.txt
+export IMAGESET_TRAIN_2012=datasets/VOCdevkit/VOC2012/ImageSets/Main/train.txt
+export IMAGESET_TRAINVAL_2007=datasets/VOCdevkit/VOC2007/ImageSets/Main/trainval.txt
+export IMAGESET_TRAINVAL_2012=datasets/VOCdevkit/VOC20012/ImageSets/Main/trainval.txt
+export IMAGESET_TEST_2007=datasets/VOCdevkit/VOC2007/ImageSets/Main/test.txt
 
 #export IMAGES_DIR_2007=~/VOCdevkit/VOC2007/JPEGImages
 #export IMAGES_DIR_2007_TEST=~/VOCdevkit/VOC2007/JPEGImages
@@ -17,12 +24,12 @@ export ANNOT_DIR_2012=/specific/netapp5_2/gamir/datasets/VOCdevkit/VOC2012/Annot
 #export ANNOT_DIR_2012=~/VOCdevkit/VOC2012/Annotations
 
 
-export IMAGESET_TRAIN_2007=~/VOCdevkit/VOC2007/ImageSets/Main/train.txt
-export IMAGESET_TRAIN_2012=~/VOCdevkit/VOC2012/ImageSets/Main/train.txt
-export IMAGESET_TRAINVAL_2007=~/VOCdevkit/VOC2007/ImageSets/Main/trainval.txt
-export IMAGESET_TRAINVAL_2012=~/VOCdevkit/VOC20012/ImageSets/Main/trainval.txt
-export IMAGESET_TEST_2007=~/VOCdevkit/VOC2007/ImageSets/Main/test.txt
+#export IMAGESET_TRAIN_2007=~/VOCdevkit/VOC2007/ImageSets/Main/train.txt
+#export IMAGESET_TRAIN_2012=~/VOCdevkit/VOC2012/ImageSets/Main/train.txt
+#export IMAGESET_TRAINVAL_2007=~/VOCdevkit/VOC2007/ImageSets/Main/trainval.txt
+#export IMAGESET_TRAINVAL_2012=~/VOCdevkit/VOC20012/ImageSets/Main/trainval.txt
+#export IMAGESET_TEST_2007=~/VOCdevkit/VOC2007/ImageSets/Main/test.txt
 
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=1,2,3
 
 /home/gamir/edocohen/virtualenv/bone_cell_py36_env/bin/python ssd300_training.py
--- ssd_keras-master/ssd300_training.py
+++ ssd_keras-master/ssd300_training.py
@@ -26,32 +26,37 @@ from data_generator.object_detection_2d_misc_utils import apply_inverse_transfor
 
 ## 1. Set the model configuration parameters
 
-img_height = 300 # Height of the model input images
-img_width = 300 # Width of the model input images
-img_channels = 3 # Number of color channels of the model input images
-mean_color = [123, 117, 104] # The per-channel mean of the images in the dataset. Do not change this value if you're using any of the pre-trained weights.
-swap_channels = [2, 1, 0] # The color channel order in the original SSD is BGR, so we'll have the model reverse the color channel order of the input images.
-n_classes = 20 # Number of positive classes, e.g. 20 for Pascal VOC, 80 for MS COCO
-scales_pascal = [0.1, 0.2, 0.37, 0.54, 0.71, 0.88, 1.05] # The anchor box scaling factors used in the original SSD300 for the Pascal VOC datasets
-scales_coco = [0.07, 0.15, 0.33, 0.51, 0.69, 0.87, 1.05] # The anchor box scaling factors used in the original SSD300 for the MS COCO datasets
+img_height = 300  # Height of the model input images
+img_width = 300  # Width of the model input images
+img_channels = 3  # Number of color channels of the model input images
+mean_color = [123, 117,
+              104]  # The per-channel mean of the images in the dataset. Do not change this value if you're using any of the pre-trained weights.
+swap_channels = [2, 1,
+                 0]  # The color channel order in the original SSD is BGR, so we'll have the model reverse the color channel order of the input images.
+n_classes = 20  # Number of positive classes, e.g. 20 for Pascal VOC, 80 for MS COCO
+scales_pascal = [0.1, 0.2, 0.37, 0.54, 0.71, 0.88,
+                 1.05]  # The anchor box scaling factors used in the original SSD300 for the Pascal VOC datasets
+scales_coco = [0.07, 0.15, 0.33, 0.51, 0.69, 0.87,
+               1.05]  # The anchor box scaling factors used in the original SSD300 for the MS COCO datasets
 scales = scales_pascal
 aspect_ratios = [[1.0, 2.0, 0.5],
-                 [1.0, 2.0, 0.5, 3.0, 1.0/3.0],
-                 [1.0, 2.0, 0.5, 3.0, 1.0/3.0],
-                 [1.0, 2.0, 0.5, 3.0, 1.0/3.0],
+                 [1.0, 2.0, 0.5, 3.0, 1.0 / 3.0],
+                 [1.0, 2.0, 0.5, 3.0, 1.0 / 3.0],
+                 [1.0, 2.0, 0.5, 3.0, 1.0 / 3.0],
                  [1.0, 2.0, 0.5],
-                 [1.0, 2.0, 0.5]] # The anchor box aspect ratios used in the original SSD300; the order matters
+                 [1.0, 2.0, 0.5]]  # The anchor box aspect ratios used in the original SSD300; the order matters
 two_boxes_for_ar1 = True
-steps = [8, 16, 32, 64, 100, 300] # The space between two adjacent anchor box center points for each predictor layer.
-offsets = [0.5, 0.5, 0.5, 0.5, 0.5, 0.5] # The offsets of the first anchor box center points from the top and left borders of the image as a fraction of the step size for each predictor layer.
-clip_boxes = False # Whether or not to clip the anchor boxes to lie entirely within the image boundaries
-variances = [0.1, 0.1, 0.2, 0.2] # The variances by which the encoded target coordinates are divided as in the original implementation
+steps = [8, 16, 32, 64, 100, 300]  # The space between two adjacent anchor box center points for each predictor layer.
+offsets = [0.5, 0.5, 0.5, 0.5, 0.5,
+           0.5]  # The offsets of the first anchor box center points from the top and left borders of the image as a fraction of the step size for each predictor layer.
+clip_boxes = False  # Whether or not to clip the anchor boxes to lie entirely within the image boundaries
+variances = [0.1, 0.1, 0.2,
+             0.2]  # The variances by which the encoded target coordinates are divided as in the original implementation
 normalize_coords = True
-
 
 ## 2. Build or load the model
 
-K.clear_session() # Clear previous models from memory.
+K.clear_session()  # Clear previous models from memory.
 
 model = ssd_300(image_size=(img_height, img_width, img_channels),
                 n_classes=n_classes,
@@ -79,14 +84,12 @@ model.load_weights(weights_path, by_name=True)
 #    If you want to follow the original Caffe implementation, use the preset SGD
 #    optimizer, otherwise I'd recommend the commented-out Adam optimizer.
 
-#adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
+# adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
 sgd = SGD(lr=0.001, momentum=0.9, decay=0.0, nesterov=False)
 
 ssd_loss = SSDLoss(neg_pos_ratio=3, alpha=1.0)
 
 model.compile(optimizer=sgd, loss=ssd_loss.compute_loss)
-
-
 
 ## 3. Set up the data generators for the training
 
@@ -102,23 +105,25 @@ val_dataset = DataGenerator(load_images_into_memory=False, hdf5_dataset_path=Non
 # TODO: Set the paths to the datasets here.
 
 # The directories that contain the images.
-VOC_2007_images_dir      = getenv('IMAGES_DIR_2007', 'datasets/VOCdevkit/VOC2007/JPEGImages/')
-VOC_2012_images_dir      = getenv('IMAGES_DIR_2012', 'datasets/VOCdevkit/VOC2012/JPEGImages/')
+VOC_2007_images_dir = getenv('IMAGES_DIR_2007', 'datasets/VOCdevkit/VOC2007/JPEGImages/')
+VOC_2012_images_dir = getenv('IMAGES_DIR_2012', 'datasets/VOCdevkit/VOC2012/JPEGImages/')
 VOC_2007_TEST_images_dir = getenv('IMAGES_DIR_2007_TEST', 'datasets/VOCdevkit/VOC2007/JPEGImages/')
 
 # The directories that contain the annotations.
-VOC_2007_annotations_dir      = getenv('ANNOT_DIR_2007','datasets/VOCdevkit/VOC2007/Annotations/')
-VOC_2012_annotations_dir      = getenv('ANNOT_DIR_2012','datasets/VOCdevkit/VOC2012/Annotations/')
-VOC_2007_TEST_annotations_dir = getenv('ANNOT_DIR_2007_TEST','datasets/VOCdevkit/VOC2007/Annotations/')
+VOC_2007_annotations_dir = getenv('ANNOT_DIR_2007', 'datasets/VOCdevkit/VOC2007/Annotations/')
+VOC_2012_annotations_dir = getenv('ANNOT_DIR_2012', 'datasets/VOCdevkit/VOC2012/Annotations/')
+VOC_2007_TEST_annotations_dir = getenv('ANNOT_DIR_2007_TEST', 'datasets/VOCdevkit/VOC2007/Annotations/')
 
 # The paths to the image sets.
-VOC_2007_train_image_set_filename    = getenv('IMAGESET_TRAIN_2007', 'datasets/VOCdevkit/VOC2007/ImageSets/Main/train.txt')
-VOC_2012_train_image_set_filename    = getenv('IMAGESET_TRAIN_2012', 'datasets/VOCdevkit/VOC2012/ImageSets/Main/train.txt')
-VOC_2007_val_image_set_filename      = getenv('IMAGESET_VAL_2007', 'datasets/VOCdevkit/VOC2007/ImageSets/Main/val.txt')
-VOC_2012_val_image_set_filename      = getenv('IMAGESET_VAL_2012', 'datasets/VOCdevkit/VOC2012/ImageSets/Main/val.txt')
-VOC_2007_trainval_image_set_filename = getenv('IMAGESET_TRAINVAL_2007', 'datasets/VOCdevkit/VOC2007/ImageSets/Main/trainval.txt')
-VOC_2012_trainval_image_set_filename = getenv('IMAGESET_TRAINVAL_2012', 'datasets/VOCdevkit/VOC2012/ImageSets/Main/trainval.txt')
-VOC_2007_test_image_set_filename     = getenv('IMAGESET_TEST_2007', 'datasets/VOCdevkit/VOC2007/ImageSets/Main/test.txt')
+VOC_2007_train_image_set_filename = getenv('IMAGESET_TRAIN_2007', 'datasets/VOCdevkit/VOC2007/ImageSets/Main/train.txt')
+VOC_2012_train_image_set_filename = getenv('IMAGESET_TRAIN_2012', 'datasets/VOCdevkit/VOC2012/ImageSets/Main/train.txt')
+VOC_2007_val_image_set_filename = getenv('IMAGESET_VAL_2007', 'datasets/VOCdevkit/VOC2007/ImageSets/Main/val.txt')
+VOC_2012_val_image_set_filename = getenv('IMAGESET_VAL_2012', 'datasets/VOCdevkit/VOC2012/ImageSets/Main/val.txt')
+VOC_2007_trainval_image_set_filename = getenv('IMAGESET_TRAINVAL_2007',
+                                              'datasets/VOCdevkit/VOC2007/ImageSets/Main/trainval.txt')
+VOC_2012_trainval_image_set_filename = getenv('IMAGESET_TRAINVAL_2012',
+                                              'datasets/VOCdevkit/VOC2012/ImageSets/Main/trainval.txt')
+VOC_2007_test_image_set_filename = getenv('IMAGESET_TEST_2007', 'datasets/VOCdevkit/VOC2007/ImageSets/Main/test.txt')
 
 # The XML parser needs to now what object class names to look for and in which order to map them to integers.
 classes = ['background',
@@ -177,7 +182,7 @@ val_dataset.parse_xml(images_dirs=[VOC_2007_TEST_images_dir],
 
 # 3: Set the batch size.
 
-batch_size = 32 # Change the batch size if you like, or if you run into GPU memory issues.
+batch_size = 32  # Change the batch size if you like, or if you run into GPU memory issues.
 
 # 4: Set the image transformations for pre-processing and data augmentation options.
 
@@ -237,7 +242,7 @@ val_generator = val_dataset.generate(batch_size=batch_size,
 
 # Get the number of samples in the training and validations datasets.
 train_dataset_size = train_dataset.get_dataset_size()
-val_dataset_size   = val_dataset.get_dataset_size()
+val_dataset_size = val_dataset.get_dataset_size()
 
 print("Number of images in the training dataset:\t{:>6}".format(train_dataset_size))
 print("Number of images in the validation dataset:\t{:>6}".format(val_dataset_size))
@@ -253,17 +258,19 @@ def lr_schedule(epoch):
     else:
         return 0.00001
 
+
 # Define model callbacks.
 
 # TODO: Set the filepath under which you want to save the model.
-model_checkpoint = ModelCheckpoint(filepath='ssd300_pascal_07+12_epoch-{epoch:02d}_loss-{loss:.4f}_val_loss-{val_loss:.4f}.h5',
-                                   monitor='val_loss',
-                                   verbose=1,
-                                   save_best_only=True,
-                                   save_weights_only=False,
-                                   mode='auto',
-                                   period=1)
-#model_checkpoint.best =
+model_checkpoint = ModelCheckpoint(
+    filepath='ssd300_pascal_07+12_epoch-{epoch:02d}_loss-{loss:.4f}_val_loss-{val_loss:.4f}.h5',
+    monitor='val_loss',
+    verbose=1,
+    save_best_only=True,
+    save_weights_only=False,
+    mode='auto',
+    period=1)
+# model_checkpoint.best =
 
 csv_logger = CSVLogger(filename='ssd300_pascal_07+12_training_log.csv',
                        separator=',',
@@ -279,16 +286,15 @@ callbacks = [model_checkpoint,
              learning_rate_scheduler,
              terminate_on_nan]
 
-
 # If you're resuming a previous training, set `initial_epoch` and `final_epoch` accordingly.
-initial_epoch   = 0
-final_epoch     = 120
-steps_per_epoch = 1000
+initial_epoch = int(getenv("INITIAL_EPOCH", 0))
+final_epoch = int(getenv("FINAL_EPOCH", 120))
+steps_per_epoch = int(getenv("STEPS_PER_EPOCH", 1000))
 
 history = model.fit_generator(generator=train_generator,
                               steps_per_epoch=steps_per_epoch,
                               epochs=final_epoch,
                               callbacks=callbacks,
                               validation_data=val_generator,
-                              validation_steps=ceil(val_dataset_size/batch_size),
+                              validation_steps=ceil(val_dataset_size / batch_size),
                               initial_epoch=initial_epoch)
