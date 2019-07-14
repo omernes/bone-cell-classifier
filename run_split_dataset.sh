0712fd59cb333cbc25534356721759c830625bb8
omer <omer@lisuto.com>
2019-07-14 15:47:27 +0300

    fix

--- run_split_dataset.sh
+++ run_split_dataset.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-export IMAGES_DIR=/a/home/cc/students/csguests/omernestor/raw_data
-export ANNOTATIONS_DIR=/a/home/cc/students/csguests/omernestor/raw_data
+export IMAGES_DIR=/a/home/cc/students/csguests/omernestor/data_xml/images
+export ANNOTATIONS_DIR=/a/home/cc/students/csguests/omernestor/data_xml/annotations
 export TARGET_DIR=/a/home/cc/students/csguests/omernestor/data_xml
 
 /home/gamir/edocohen/anaconda3/envs/bone_cell_py37/bin/python split_dataset.py
