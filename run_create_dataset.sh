d1c34a0a81661a741cf55784b69e2415639c3b37
omer <omer@lisuto.com>
2019-07-14 15:05:52 +0300

    create dataset

---
+++ run_create_dataset.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+export IMAGES_DIR=/a/home/cc/students/csguests/omernestor/raw_data
+export ANNOTATIONS_DIR=/a/home/cc/students/csguests/omernestor/raw_data
+
+export TARGET_IMAGES_DIR=/a/home/cc/students/csguests/omernestor/data_xml/images
+export TARGET_ANNOTATIONS_DIR=/a/home/cc/students/csguests/omernestor/data_xml/annotations
+
+/home/gamir/edocohen/anaconda3/envs/bone_cell_py37/bin/python create_dataset_pascalvoc.py
