850798339d0c37866ce6e0886d8b876aa50b6038
omer <omer@lisuto.com>
2019-08-31 14:00:16 +0300

    final version

--- run_subsample_weights.sh
+++ run_subsample_weights.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-WEIGHTS_SOURCE_ZIP_PATH="data/VGG_VOC0712_SSD_300x300_iter_120000.zip"
+WEIGHTS_SOURCE_ZIP_PATH="ssd_keras/data/VGG_VOC0712_SSD_300x300_iter_120000.zip"
 unzip $WEIGHTS_SOURCE_ZIP_PATH
 
 /home/gamir/edocohen/anaconda3/envs/bone_cell_py37/bin/python ssd_keras/create_subsampled_weights.py
