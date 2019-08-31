394547d34964ba59d77177cbf3bb6278a83795ba
omer <omer@lisuto.com>
2019-08-31 14:36:45 +0300

    final version

--- run_finetuning.sh
+++ run_finetuning.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+./run_subsample_weights.sh
+
 export IMAGES_DIR=/a/home/cc/students/csguests/omernestor/data_xml/images
 export ANNOTATIONS_DIR=/a/home/cc/students/csguests/omernestor/data_xml/annotations
 
@@ -7,7 +9,7 @@ export TRAIN_IMAGESET_FILENAME=/a/home/cc/students/csguests/omernestor/data_xml/
 export VAL_IMAGESET_FILENAME=/a/home/cc/students/csguests/omernestor/data_xml/val.txt
 export TEST_IMAGESET_FILENAME=/a/home/cc/students/csguests/omernestor/data_xml/test.txt
 
-export WEIGHTS_PATH=data/VGG_VOC0712_SSD_300x300_iter_120000_subsampled_6_classes.h5
+export WEIGHTS_PATH=ssd_keras/data/VGG_VOC0712_SSD_300x300_iter_120000_subsampled_6_classes.h5
 export SAVE_IN_MEMORY=1
 export MODEL_PATH=""
 export NUM_CLASSES=5
